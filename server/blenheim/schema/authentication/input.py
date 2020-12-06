e97ea4d9a6a4ac0b19b3cf3d0fc7605bb4111f25
Richie Ward <RichieS@GMail.com>
2020-12-06 16:07:36 +0000

    Remove server

--- server/blenheim/schema/authentication/input.py
+++
@@ -1,7 +0,0 @@
-import graphene
-from graphene import InputObjectType, String, NonNull
-
-
-class UserInput(InputObjectType):
-    name = String(required=True)
-    password = NonNull(String)
--- server/blenheim/template/dns/__init__.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname, join, exists
-
-
-def get_zonefile():
-    return join(dirname(__file__), 'zonefile.jinja2')
