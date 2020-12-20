61714eb20c3a2c58163061724c6d5c4af60dfa5a
Richie Ward <RichieS@GMail.com>
2020-12-20 00:34:06 +0000

    Convert to react hook

--- src/components/nav/Generate.tsx
+++ src/components/nav/Generate.tsx
@@ -1,19 +1,20 @@
-import React, { FunctionComponent, useContext, useState } from "react";
-import { Box, Button, Theme, Typography } from "@material-ui/core";
-import { QUERY } from "../queries/DnsQuery";
-import { StoreProvider } from "../../StoreProvider";
-import Query from 'react-apollo/Query';
-import { Dns, DnsVariables } from "../../types/Dns";
-import { SingleButtonDialog } from "../dialogs/generic/SingleButtonDialog";
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
+import { Box, Button, Theme, Typography } from '@material-ui/core';
+import { QUERY } from '../queries/DnsQuery';
+import { StoreProvider } from '../../StoreProvider';
+import { Dns, DnsVariables } from '../../types/Dns';
+import { SingleButtonDialog } from '../dialogs/generic/SingleButtonDialog';
 import ErrorIcon from '@material-ui/icons/Error';
 import InfoIcon from '@material-ui/icons/Info';
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import { useLazyQuery } from '@apollo/client';
 
 const useStyles = makeStyles<Theme, {}>((theme) => {
     return ({
         icon: {
-            paddingRight: theme.spacing(1),
-        },
+            paddingRight: theme.spacing(1)
+        }
     });
 });
 
@@ -23,12 +24,40 @@ export const Generate: FunctionComponent = () => {
     const [dialogOpen, setDialogOpen] = useState(false);
     const [result, setResult] = useState('');
     const [error, setError] = useState(false);
+    const [getDNS, { loading, error: queryError, data }] = useLazyQuery<Dns, DnsVariables>(
+        QUERY,
+        { variables: { token: store.token }, fetchPolicy: 'no-cache' }
+    );
+
+    if (loading) {
+        setError(false);
+        setResult('Loading...');
+    }
+
+    if (queryError) {
+        setError(true);
+        setResult(queryError.message || '');
+    }
+
+    if (data && data.dns.generate.success) {
+        setError(false);
+        setResult('Generated.');
+    } else if (data && !data.dns.generate.success) {
+        setError(true);
+        setResult((data && data.dns.generate.error) || '');
+    }
+
+    useEffect(() => {
+        if (dialogOpen) {
+            getDNS();
+        }
+    }, [dialogOpen]);
 
     return (
         <>
             <SingleButtonDialog
                 dialogOpen={dialogOpen}
-                okClicked={() => {}}
+                okClicked={() => { }}
                 onClose={() => {
                     setDialogOpen(false);
                     setError(false);
@@ -39,37 +68,11 @@ export const Generate: FunctionComponent = () => {
                     <>
                         <Typography variant='subtitle1'>Generating configuration files...</Typography>
                         <Box p={1} />
-                        {dialogOpen && (
-                            <Query<Dns, DnsVariables>
-                                query={QUERY}
-                                variables={{token: store.token}}
-                                fetchPolicy='no-cache'
-                            >
-                                {({ loading, error, data }): JSX.Element => {
-                                    if (error) {
-                                        setError(true);
-                                        setResult(error.message || '');
-                                        return (<></>);
-                                    }
-                                    if (loading) {
-                                        return (<></>);
-                                    }
-                                    if (data && data.dns.generate.success) {
-                                        setError(false);
-                                        setResult('Generated.');
-                                    } else {
-                                        setError(true);
-                                        setResult((data && data.dns.generate.error) || '');
-                                    }
-                                    return (<></>);
-                                }}
-                            </Query>
-                        )}
                         {dialogOpen && result.length > 0 && (
                             <Typography variant='subtitle1' color={error ? 'error' : undefined}>
-                                {error ?
-                                    <ErrorIcon color='error' className={classes.icon} /> :
-                                    <InfoIcon color='primary' className={classes.icon} />
+                                {error
+                                    ? <ErrorIcon color='error' className={classes.icon} />
+                                    : <InfoIcon color='primary' className={classes.icon} />
                                 }
                                 {result}
                             </Typography>
