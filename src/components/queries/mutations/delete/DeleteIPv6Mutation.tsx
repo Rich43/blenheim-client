81d80ff97d2bb94345b4f349736d2393f4d0fd7e
Richie Ward <RichieS@GMail.com>
2020-12-20 14:25:02 +0000

    Add DeleteIPv6Mutation.tsx type

--- src/components/queries/mutations/delete/DeleteIPv6Mutation.tsx
+++ src/components/queries/mutations/delete/DeleteIPv6Mutation.tsx
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import { DeleteIPv6, DeleteIPv6Variables } from '../../../../types/DeleteIPv6';
-import { useMutation } from '@apollo/client';
+import { MutationTuple, useMutation } from '@apollo/client';
 
 const MUTATION = gql`
     mutation DeleteIPv6($token: String!, $index: Int!) {
@@ -17,4 +17,5 @@ const MUTATION = gql`
     }
 `;
 
-export const useDeleteIPv6Mutation = () => useMutation<DeleteIPv6, DeleteIPv6Variables>(MUTATION);
+type DeleteIPv6Mutation = () => MutationTuple<DeleteIPv6, DeleteIPv6Variables>;
+export const useDeleteIPv6Mutation: DeleteIPv6Mutation = () => useMutation<DeleteIPv6, DeleteIPv6Variables>(MUTATION);
