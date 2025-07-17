8e3919a1ccb5874e6bc6a9d679f06bd4bd03d16c
Richard Ward <richard.ward637@outlook.com>
2025-07-17 10:02:05 +0100

    fix cypress component testing setup

--- cypress/component/sideDrawer.cy.tsx
+++ cypress/component/sideDrawer.cy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'cypress/react18';
+import { mount } from 'cypress/react';
 import { MemoryRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { SideDrawer } from '../../src/components/nav/SideDrawer';
